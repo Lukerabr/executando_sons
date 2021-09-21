d7a200c2b4ebf3e198c5c71e5d7cbe0b85e6370f
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-09-21 19:00:36 -0400

    Executar, pausar e parar.

--- lib/main.dart
+++ lib/main.dart
@@ -20,24 +20,45 @@ class _HomeState extends State<Home> {
 
   AudioPlayer audioPlayer = AudioPlayer();
   AudioCache audioCache = AudioCache(prefix: "audios/");
+  bool primeiraExecucao = true;
 
 
   _executar_local() async {
-
-    audioPlayer = await audioCache.play("musica.mp3");
+    if(primeiraExecucao){
+      audioPlayer = await audioCache.play("musica.mp3");
+      primeiraExecucao = false;
+    }else{
+      audioPlayer.resume();
+    }
 
   }
 
-  _executar_player() async {
-    String url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
-    int resultado = await audioPlayer.play(url);
-
+  _pausar_local() async {
+    int resultado = await audioPlayer.pause();
     if(resultado == 1){
       //sucesso
     }else{
+    }
+  }
 
+  _parar_local() async {
+
+    int resultado = await audioPlayer.stop();
+    if(resultado == 1){
+      //sucesso
+    }else{
     }
 
+  }
+
+
+  _executar_url() async {
+    String url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+    int resultado = await audioPlayer.play(url);
+    if(resultado == 1){
+      //sucesso
+    }else{
+    }
   }
 
 
@@ -47,6 +68,51 @@ class _HomeState extends State<Home> {
 
     //_executar_player();
 
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Executando sons"),
+      ),
+      body: Column(
+        children: [
+          //Slider
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Padding(
+                  padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  child: Image.asset("assets/imagens/executar.png"),
+                  onTap: (){
+                    _executar_local();
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  child: Image.asset("assets/imagens/pausar.png"),
+                  onTap: (){
+                    _pausar_local();
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  child: Image.asset("assets/imagens/parar.png"),
+                  onTap: (){
+                    _parar_local();
+                  },
+                ),
+              )
+
+
+
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
